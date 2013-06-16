d52cc5f7191ac4f4cbc3ae9c8920be1e4981e333
glynnforrest <me@glynnforrest.com>
2013-06-17 00:26:07 +0100

    Adding DotArray implementation.

--- src/Crutches/DotArray.php
+++ src/Crutches/DotArray.php
@@ -3,15 +3,80 @@
 namespace Crutches;
 
 /**
- * DotArray
+ * DotArray provides an interface to an array using the
+ * 'dot array syntax', i.e
+ *
+ * $arr->get('parent.child.child') => $arr['parent']['child']['child']
+ *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class DotArray {
 
-    protected $array = array();
+	protected $array = array();
 
-    public function __construct(array $array = array()) {
-            $this->array = $array;
-    }
+	public function __construct(array $array = array()) {
+		$this->array = $array;
+	}
+
+	/**
+	 * Get a value from the array that matches $key.
+	 * $key uses the dot array syntax: parent.child.child
+	 * If the key matches an array the whole array will be returned.
+	 * If no key is specified the entire array will be returned.
+	 * $default will be returned (null unless specified) if the key is
+	 * not found.
+	 */
+	public function get($key = null, $default = null) {
+		if(!$key) {
+			return $this->array;
+		}
+		$parts = explode('.', $key);
+		$scope = &$this->array;
+		for ($i = 0; $i < count($parts) - 1; $i++) {
+			if(!isset($scope[$parts[$i]])) {
+				return $default;
+			}
+			$scope = &$scope[$parts[$i]];
+		}
+		if(isset($scope[$parts[$i]])) {
+			return $scope[$parts[$i]];
+		}
+		return $default;
+	}
+
+	/**
+	 * Get the first value from an array of values that matches $key.
+	 * $default will be returned (null unless specified) if the key is
+	 * not found or does not contain an array.
+	 */
+	public function getFirst($key = null, $default = null) {
+		$array = self::get($key);
+		if(!is_array($array)) {
+			return $default;
+		}
+		reset($array);
+		return current($array);
+	}
+
+	/**
+	 * Set an array value with $key.
+	 * $key uses the dot array syntax: parent.child.child.
+	 * If $value is an array this will also be accessible using the
+	 * dot array syntax.
+	 */
+	public function set($key, $value) {
+		$parts = explode('.', $key);
+		//loop through each part, create it if not present.
+		$scope = &$this->array;
+		$count = count($parts) - 1;
+		for ($i = 0; $i < $count; $i++) {
+			if(!isset($scope[$parts[$i]])) {
+				$scope[$parts[$i]] = array();
+			}
+			$scope = &$scope[$parts[$i]];
+		}
+		$scope[$parts[$i]] = $value;
+		$this->modified = true;
+	}
 
 }
--- tests/Crutches/Tests/DotArrayTest.php
+++ tests/Crutches/Tests/DotArrayTest.php
@@ -8,24 +8,119 @@ include(__DIR__ . '/../../bootstrap.php');
 
 /**
  * DotArrayTest
+ *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
 class DotArrayTest extends \PHPUnit_Framework_TestCase {
 
-    protected $arr1 = array(
+	protected $arr = array(
+		'one' => 1,
+		'two' => array(
+			'one' => 2.1,
+			'two' => 2.2
+		));
+
+	public function testConstruct() {
+		$d = new DotArray();
+		$this->assertTrue($d instanceof DotArray);
+		$e = new DotArray($this->arr);
+		$this->assertTrue($e instanceof DotArray);
+	}
+
+	public function testGet() {
+		$c = new DotArray($this->arr);
+		$this->assertEquals(1, $c->get('one'));
+		$this->assertEquals(2.1, $c->get('two.one'));
+		$expected = array(
 			'one' => 1,
 			'two' => array(
 				'one' => 2.1,
 				'two' => 2.2
-			));
+			)
+		);
+		$this->assertEquals($expected, $c->get());
+	}
 
-    public function testConstruct() {
-        $d = new DotArray();
-        $this->assertTrue($d instanceof DotArray);
-        $e = new DotArray($this->arr1);
-        $this->assertTrue($e instanceof DotArray);
-    }
+	public function testGetDefault() {
+		$c = new DotArray($this->arr);
+		$this->assertEquals('default', $c->get('fake-key', 'default'));
+		$expected = array(
+			'one' => 1,
+			'two' => array(
+				'one' => 2.1,
+				'two' => 2.2
+			)
+		);
+		$this->assertEquals($expected, $c->get(null, 'default'));
+	}
 
+	public function testGetFirst() {
+		$c = new DotArray($this->arr);
+		$this->assertEquals(2.1, $c->getFirst('two'));
+		$this->assertEquals(1, $c->getFirst());
+	}
 
+	public function testGetFirstDefault() {
+		$c = new DotArray($this->arr);
+		$this->assertEquals('default', $c->getFirst('fake-key', 'default'));
+	}
+
+	public function testSet() {
+		$c = new DotArray($this->arr);
+		$c->set('three', 3);
+		$this->assertEquals(3, $c->get('three'));
+	}
+
+	public function testSetNoFile() {
+		$c = new DotArray($this->arr);
+		$c->set('ad-hoc', 'data');
+		$this->assertEquals('data', $c->get('ad-hoc'));
+		$c->set('nested', array('value' => 'foo'));
+		$this->assertEquals('foo', $c->get('nested.value'));
+	}
+
+	public function testSetNested() {
+		$c = new DotArray();
+		$c->set('parent.child', 'value');
+		$this->assertEquals(array('parent' => array('child' => 'value')), $c->get());
+	}
+
+	public function testGetNested() {
+		$c = new DotArray($this->arr);
+		$c->set('parent', array('child' => 'value'));
+		$this->assertEquals('value', $c->get('parent.child'));
+	}
+
+	public function testSetDeepNested() {
+		$c = new DotArray();
+		$c->set('parent.child.0.1.2.3.4', 'value');
+		$this->assertEquals(array('parent' => array('child' => array(
+			0 => array(1 => array(2 => array(3 => array(4 =>'value'))))))), $c->get());
+	}
+
+	public function testGetDeepNested() {
+		$c = new DotArray($this->arr);
+		$c->set('parent', array('child' => array(
+			0 => array(1 => array(2 => array(3 => array(4 =>'value')))))));
+		$this->assertEquals('value', $c->get('parent.child.0.1.2.3.4'));
+	}
+
+	public function testEmptyGet() {
+		$c = new DotArray($this->arr);
+		$this->assertEquals(array(
+			'one' => 1,
+			'two' => array(
+				'one' => 2.1,
+				'two' => 2.2
+			)
+		), $c->get());
+		$this->assertEquals(array(
+			'one' => 1,
+			'two' => array(
+				'one' => 2.1,
+				'two' => 2.2
+			)
+		), $c->get(null));
+	}
 
 }
