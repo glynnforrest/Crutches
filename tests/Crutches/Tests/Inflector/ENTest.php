c105dc1650006351597966391cf4cce2422390c8
Glynn Forrest <me@glynnforrest.com>
2013-11-27 16:16:11 +0000

    Cleaning up ENTest.

--- tests/Crutches/Tests/Inflector/ENTest.php
+++ tests/Crutches/Tests/Inflector/ENTest.php
@@ -13,58 +13,47 @@ include(__DIR__ . '/../../../bootstrap.php');
  **/
 class ENTest extends \PHPUnit_Framework_TestCase {
 
-	public function testPlural() {
-		$inf = new EN();
-		$this->assertEquals('cats', $inf->plural('cats'));
-		$this->assertEquals('cats', $inf->plural('cat'));
-		$this->assertEquals('mice', $inf->plural('mouse'));
-		$this->assertEquals('lice', $inf->plural('louse'));
-		$this->assertEquals('houses', $inf->plural('house'));
-		$this->assertEquals('buses', $inf->plural('bus'));
-		$this->assertEquals('sheep', $inf->plural('sheep'));
-		$this->assertEquals('cacti', $inf->plural('cactus'));
-		$this->assertEquals('octopi', $inf->plural('octopus'));
-		$this->assertEquals('fish', $inf->plural('fish'));
-		$this->assertEquals('wishes', $inf->plural('wish'));
-		$this->assertEquals('boxes', $inf->plural('box'));
-		$this->assertEquals('witches', $inf->plural('witch'));
-		$this->assertEquals('jeans', $inf->plural('jeans'));
-		$this->assertEquals('scissors', $inf->plural('scissors'));
-		$this->assertEquals('curries', $inf->plural('curry'));
-		$this->assertEquals('flies', $inf->plural('fly'));
-		$this->assertEquals('pringles', $inf->plural('pringle'));
-		$this->assertEquals('tomatoes', $inf->plural('tomato'));
-		$this->assertEquals('monkeys', $inf->plural('monkey'));
-		$this->assertEquals('shoes', $inf->plural('shoe'));
-		$this->assertEquals('messages', $inf->plural('message'));
-		$this->assertEquals('geese', $inf->plural('goose'));
+	public function pluralsProvider() {
+		return array(
+			array('cat', 'cats'),
+			array('mouse', 'mice'),
+			array('louse', 'lice'),
+			array('house', 'houses'),
+			array('bus', 'buses'),
+			array('sheep', 'sheep'),
+			array('cactus', 'cacti'),
+			array('octopus', 'octopi'),
+			array('fish', 'fish'),
+			array('wish', 'wishes'),
+			array('box', 'boxes'),
+			array('witch', 'witches'),
+			array('jeans', 'jeans'),
+			array('scissors', 'scissors'),
+			array('curry', 'curries'),
+			array('fly', 'flies'),
+			array('pringle', 'pringles'),
+			array('tomato', 'tomatoes'),
+			array('monkey', 'monkeys'),
+			array('shoe', 'shoes'),
+			array('message', 'messages'),
+			array('goose', 'geese'),
+		);
 	}
 
-	public function testSingle() {
+	/**
+	 * @dataProvider pluralsProvider()
+	 */
+	public function testPlural($singular, $plural) {
 		$inf = new EN();
-		$this->assertEquals('cat', $inf->single('cat'));
-		$this->assertEquals('cat', $inf->single('cats'));
-		$this->assertEquals('mouse', $inf->single('mice'));
-		$this->assertEquals('louse', $inf->single('lice'));
-		$this->assertEquals('house', $inf->single('houses'));
-		$this->assertEquals('bus', $inf->single('buses'));
-		$this->assertEquals('sheep', $inf->single('sheep'));
-		$this->assertEquals('cactus', $inf->single('cacti'));
-		$this->assertEquals('octopus', $inf->single('octopi'));
-		$this->assertEquals('fish', $inf->single('fish'));
-		$this->assertEquals('wish', $inf->single('wishes'));
-		$this->assertEquals('box', $inf->single('boxes'));
-		$this->assertEquals('witch', $inf->single('witches'));
-		$this->assertEquals('jeans', $inf->single('jeans'));
-		$this->assertEquals('scissors', $inf->single('scissors'));
-		$this->assertEquals('curry', $inf->single('curries'));
-		$this->assertEquals('fly', $inf->single('flies'));
-		$this->assertEquals('pringle', $inf->single('pringles'));
-		$this->assertEquals('tomato', $inf->single('tomatoes'));
-		$this->assertEquals('monkey', $inf->single('monkeys'));
-		$this->assertEquals('shoe', $inf->single('shoes'));
-		$this->assertEquals('message', $inf->single('messages'));
-		$this->assertEquals('goose', $inf->single('geese'));
+		$this->assertSame($plural, $inf->plural($singular));
+	}
+
+	/**
+	 * @dataProvider pluralsProvider()
+	 */
+	public function testSingle($singular, $plural) {
+		$inf = new EN();
+		$this->assertSame($singular, $inf->single($plural));
 	}
 
 }
