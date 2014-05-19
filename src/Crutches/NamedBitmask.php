941c385117e1cecf1294f04c180e63d8823189b8
Glynn Forrest <me@glynnforrest.com>
2014-05-19 19:15:50 +0100

    Creating NamedBitmask for referring to flags by name.

---
+++ src/Crutches/NamedBitmask.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Crutches;
+
+/**
+ * Bitmask
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class NamedBitmask
+{
+    protected $bitmask;
+    protected $names;
+
+    /**
+     * Create a new Bitmask instance.
+     *
+     * @param int $bitmask The bitmask
+     */
+    public function __construct(array $names, $bitmask = 0)
+    {
+        $this->bitmask = $bitmask;
+        $this->setNames($names);
+    }
+
+    /**
+     * Get the bitmask held in this Bitmask instance.
+     *
+     * @return int The bitmask
+     */
+    public function getBitmask()
+    {
+        return $this->bitmask;
+    }
+
+    /**
+     * Set the bitmask held in this Bitmask instance.
+     *
+     * @param  int                   $bitmask The bitmask
+     * @return Crutches\NamedBitmask This NamedBitmask instance
+     */
+    public function setBitmask($bitmask)
+    {
+        $this->bitmask = $bitmask;
+
+        return $this;
+    }
+
+    /**
+     * Get the names of bitmask flags.
+     *
+     * @return array An array of flag names
+     */
+    public function getNames()
+    {
+        return array_keys($this->names);
+    }
+
+    /**
+     * Set the names of bitmask flags.
+     *
+     * @param  array                 $names An array of flag names
+     * @return Crutches\NamedBitmask This NamedBitmask instance
+     */
+    public function setNames(array $names, $update_bitmask = true)
+    {
+        //update the names here - take a diff of old and new names,
+        //compare values and add to new bitmask
+
+        //each name will have an incrementing bit value, e.g. an array
+        //of 3 will have values of 1, 2 and 4
+        $bits = array();
+        $bit = 1;
+        for ($i = 0; $i < count($names); $i++) {
+            $bits[] = $bit;
+            $bit *= 2;
+        }
+        $this->names = array_combine($names, $bits);
+
+        return $this;
+    }
+
+    protected function getMaskFromFlags($flags)
+    {
+        $flags = (array) $flags;
+        $mask = 0;
+        foreach ($flags as $flag) {
+            if (!isset($this->names[$flag])) {
+                return false;
+            }
+            $mask = $mask | $this->names[$flag];
+        }
+
+        return $mask;
+    }
+
+    /**
+     * Check if this Bitmask contains a given flag.
+     *
+     * Check more than one flag at a time by separating individual
+     * flags with |. For example, supplying 4 | 1 will check for both
+     * 4 and 1, and will pass only if both 4 and 1 are present.
+     *
+     * @param  mixed $flags The flag to check as a string, or multiple flags as an array
+     * @return bool  The result of the check
+     */
+    public function hasFlag($flags)
+    {
+        $mask = $this->getMaskFromFlags($flags);
+
+        return ($this->bitmask & $mask) === $mask;
+    }
+
+    /**
+     * Add a flag to this Bitmask.
+     *
+     * Add more than one flag at a time by separating individual
+     * flags with |. For example, supplying 4 | 1 will add both 4
+     * and 1.
+     *
+     * @param  mixed            $flags The flag to add as a string, or multiple flags as an array
+     * @return Crutches\Bitmask This Bitmask instance
+     */
+    public function addFlag($flags)
+    {
+        $this->bitmask = $this->bitmask | $this->getMaskFromFlags($flags);
+
+        return $this;
+    }
+
+    /**
+     * Remove a flag from this Bitmask.
+     *
+     * Remove more than one flag at a time by separating individual
+     * flags with |. For example, supplying 4 | 1 will remove both 4
+     * and 1.
+     *
+     * @param  mixed            $flags The flag to remove as a string, or multiple flags as an array
+     * @return Crutches\Bitmask This Bitmask instance
+     */
+    public function removeFlag($flags)
+    {
+        $this->bitmask = $this->bitmask & ~$this->getMaskFromFlags($flags);
+
+        return $this;
+    }
+
+}
---
+++ tests/Crutches/Tests/NamedBitmaskTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Crutches\Tests;
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+use Crutches\NamedBitmask;
+
+/**
+ * NamedBitmaskTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class NamedBitmaskTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testSetAndGetBitmask()
+    {
+        $mask = new NamedBitmask(array(), 4);
+        $this->assertSame(4, $mask->getBitmask());
+        $mask->setBitmask(5);
+        $this->assertSame(5, $mask->getBitmask());
+    }
+
+    public function testSetAndGetNames()
+    {
+        $mask = new NamedBitmask(array('ADMIN', 'MODERATOR'));
+        $this->assertSame(array('ADMIN', 'MODERATOR'), $mask->getNames());
+        $this->assertSame($mask, $mask->setNames(array('ADMIN')));
+        $this->assertSame(array('ADMIN'), $mask->getNames());
+    }
+
+    public function testHasFlag()
+    {
+        $mask = new NamedBitmask(array('ADMIN', 'MODERATOR'), 0);
+        $this->assertFalse($mask->hasFlag('ADMIN'));
+        $this->assertFalse($mask->hasFlag('MODERATOR'));
+        $this->assertFalse($mask->hasFlag(array('MODERATOR', 'ADMIN')));
+        $this->assertFalse($mask->hasFlag(array('ADMIN', 'MODERATOR')));
+
+        //add admin role
+        $mask->setBitmask(1);
+        $this->assertTrue($mask->hasFlag('ADMIN'));
+        $this->assertFalse($mask->hasFlag('MODERATOR'));
+        $this->assertFalse($mask->hasFlag(array('MODERATOR', 'ADMIN')));
+        $this->assertFalse($mask->hasFlag(array('ADMIN', 'MODERATOR')));
+
+        //add moderator role
+        $mask->setBitmask(3);
+        $this->assertTrue($mask->hasFlag('ADMIN'));
+        $this->assertTrue($mask->hasFlag('MODERATOR'));
+        $this->assertTrue($mask->hasFlag(array('MODERATOR', 'ADMIN')));
+        $this->assertTrue($mask->hasFlag(array('ADMIN', 'MODERATOR')));
+    }
+
+    public function testAddFlag()
+    {
+        $mask = new NamedBitmask(array('ADMIN', 'MODERATOR', 'EDITOR'));
+        $this->assertSame(0, $mask->getBitmask());
+        $this->assertSame($mask, $mask->addFlag('ADMIN'));
+        $this->assertSame(1, $mask->getBitmask());
+        $this->assertSame($mask, $mask->addFlag(array('MODERATOR', 'EDITOR')));
+        $this->assertSame(7, $mask->getBitmask());
+    }
+
+    public function testRemoveFlag()
+    {
+        $mask = new NamedBitmask(array('ADMIN', 'MODERATOR', 'EDITOR'), 7);
+        $this->assertSame(7, $mask->getBitmask());
+        $this->assertSame($mask, $mask->removeFlag('ADMIN'));
+        $this->assertSame(6, $mask->getBitmask());
+        $this->assertSame($mask, $mask->removeFlag(array('MODERATOR', 'EDITOR')));
+        $this->assertSame(0, $mask->getBitmask());
+    }
+
+}
