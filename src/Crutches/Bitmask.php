636b1abc49f9849aa66384d1f1a14ca1f90c9702
Glynn Forrest <me@glynnforrest.com>
2014-05-26 21:12:59 -0500

    Coding style fix for Bitmask.

--- src/Crutches/Bitmask.php
+++ src/Crutches/Bitmask.php
@@ -7,81 +7,90 @@ namespace Crutches;
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class Bitmask {
+class Bitmask
+{
+    protected $bitmask;
 
-	protected $bitmask;
+    /**
+     * Create a new Bitmask instance.
+     *
+     * @param int $bitmask The bitmask
+     */
+    public function __construct($bitmask = 0)
+    {
+        $this->bitmask = $bitmask;
+    }
 
-	/**
-	 * Create a new Bitmask instance.
-	 *
-	 * @param int $bitmask The bitmask
-	 */
-	public function __construct($bitmask = 0) {
-		$this->bitmask = $bitmask;
-	}
+    /**
+     * Get the bitmask value.
+     *
+     * @return int The bitmask
+     */
+    public function getBitmask()
+    {
+        return $this->bitmask;
+    }
 
-	/**
-	 * Get the bitmask value.
-	 *
-	 * @return int The bitmask
-	 */
-	public function getBitmask() {
-		return $this->bitmask;
-	}
+    /**
+     * Set the bitmask value.
+     *
+     * @param  int              $bitmask The bitmask
+     * @return Crutches\Bitmask This Bitmask instance
+     */
+    public function setBitmask($bitmask)
+    {
+        $this->bitmask = $bitmask;
 
-	/**
-	 * Set the bitmask value.
-	 *
-	 * @param int $bitmask The bitmask
-	 * @return Crutches\Bitmask This Bitmask instance
-	 */
-	public function setBitmask($bitmask) {
-		$this->bitmask = $bitmask;
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Check if the Bitmask contains a given flag.
-	 *
-	 * Check more than one flag at a time by separating individual
-	 * flags with |. For example, supplying 4 | 1 will check for both
-	 * 4 and 1, and will pass only if both 4 and 1 are present.
-	 *
-	 * @param int $flag The flag to check
-	 * @return bool The result of the check
-	 */
-	public function hasFlag($flag) {
-		return ($this->bitmask & $flag) === $flag;
-	}
+    /**
+     * Check if the Bitmask contains a given flag.
+     *
+     * Check more than one flag at a time by separating individual
+     * flags with |. For example, supplying 4 | 1 will check for both
+     * 4 and 1, and will pass only if both 4 and 1 are present.
+     *
+     * @param  int  $flag The flag to check
+     * @return bool The result of the check
+     */
+    public function hasFlag($flag)
+    {
+        return ($this->bitmask & $flag) === $flag;
+    }
 
-	/**
-	 * Add a flag to the Bitmask.
-	 *
-	 * Add more than one flag at a time by separating individual
-	 * flags with |. For example, supplying 4 | 1 will add both 4
-	 * and 1.
-	 *
-	 * @param int $flag The flag to add
-	 * @return Crutches\Bitmask This Bitmask instance
-	 */
-	public function addFlag($flag) {
-		$this->bitmask = $this->bitmask | $flag;
-		return $this;
-	}
+    /**
+     * Add a flag to the Bitmask.
+     *
+     * Add more than one flag at a time by separating individual
+     * flags with |. For example, supplying 4 | 1 will add both 4
+     * and 1.
+     *
+     * @param  int              $flag The flag to add
+     * @return Crutches\Bitmask This Bitmask instance
+     */
+    public function addFlag($flag)
+    {
+        $this->bitmask = $this->bitmask | $flag;
 
-	/**
-	 * Remove a flag from the Bitmask.
-	 *
-	 * Remove more than one flag at a time by separating individual
-	 * flags with |. For example, supplying 4 | 1 will remove both 4
-	 * and 1.
-	 *
-	 * @param int $flag The flag to remove
-	 * @return Crutches\Bitmask This Bitmask instance
-	 */
-	public function removeFlag($flag) {
-		$this->bitmask = $this->bitmask & ~$flag;
-		return $this;
-	}
+        return $this;
+    }
+
+    /**
+     * Remove a flag from the Bitmask.
+     *
+     * Remove more than one flag at a time by separating individual
+     * flags with |. For example, supplying 4 | 1 will remove both 4
+     * and 1.
+     *
+     * @param  int              $flag The flag to remove
+     * @return Crutches\Bitmask This Bitmask instance
+     */
+    public function removeFlag($flag)
+    {
+        $this->bitmask = $this->bitmask & ~$flag;
+
+        return $this;
+    }
 
 }
