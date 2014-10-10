d307cc272d1401fa98a9c353d0d0515b84245757
Glynn Forrest <me@glynnforrest.com>
2014-10-10 13:06:26 +0100

    Code style fix for DotArray and test.

--- src/Crutches/DotArray.php
+++ src/Crutches/DotArray.php
@@ -10,83 +10,90 @@ namespace Crutches;
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class DotArray {
+class DotArray
+{
+    protected $array = array();
 
-	protected $array = array();
+    public function __construct(array $array = array())
+    {
+        $this->array = $array;
+    }
 
-	public function __construct(array $array = array()) {
-		$this->array = $array;
-	}
+    /**
+     * Get a value from the array that matches $key.
+     * $key uses the dot array syntax: parent.child.child
+     * If the key matches an array the whole array will be returned.
+     * If no key is specified the entire array will be returned.
+     * $default will be returned (null unless specified) if the key is
+     * not found.
+     */
+    public function get($key = null, $default = null)
+    {
+        if (!$key) {
+            return $this->array;
+        }
+        $parts = explode('.', $key);
+        $scope = &$this->array;
+        for ($i = 0; $i < count($parts) - 1; $i++) {
+            if (!isset($scope[$parts[$i]])) {
+                return $default;
+            }
+            $scope = &$scope[$parts[$i]];
+        }
+        if (isset($scope[$parts[$i]])) {
+            return $scope[$parts[$i]];
+        }
 
-	/**
-	 * Get a value from the array that matches $key.
-	 * $key uses the dot array syntax: parent.child.child
-	 * If the key matches an array the whole array will be returned.
-	 * If no key is specified the entire array will be returned.
-	 * $default will be returned (null unless specified) if the key is
-	 * not found.
-	 */
-	public function get($key = null, $default = null) {
-		if(!$key) {
-			return $this->array;
-		}
-		$parts = explode('.', $key);
-		$scope = &$this->array;
-		for ($i = 0; $i < count($parts) - 1; $i++) {
-			if(!isset($scope[$parts[$i]])) {
-				return $default;
-			}
-			$scope = &$scope[$parts[$i]];
-		}
-		if(isset($scope[$parts[$i]])) {
-			return $scope[$parts[$i]];
-		}
-		return $default;
-	}
+        return $default;
+    }
 
-	/**
-	 * Get the first value from an array of values that matches $key.
-	 * $default will be returned (null unless specified) if the key is
-	 * not found or does not contain an array.
-	 */
-	public function getFirst($key = null, $default = null) {
-		$array = self::get($key);
-		if(!is_array($array)) {
-			return $default;
-		}
-		reset($array);
-		return current($array);
-	}
+    /**
+     * Get the first value from an array of values that matches $key.
+     * $default will be returned (null unless specified) if the key is
+     * not found or does not contain an array.
+     */
+    public function getFirst($key = null, $default = null)
+    {
+        $array = self::get($key);
+        if (!is_array($array)) {
+            return $default;
+        }
+        reset($array);
 
-	/**
-	 * Set an array value with $key.
-	 * $key uses the dot array syntax: parent.child.child.
-	 * If $value is an array this will also be accessible using the
-	 * dot array syntax.
-	 */
-	public function set($key, $value) {
-		$parts = explode('.', $key);
-		//loop through each part, create it if not present.
-		$scope = &$this->array;
-		$count = count($parts) - 1;
-		for ($i = 0; $i < $count; $i++) {
-			if(!isset($scope[$parts[$i]])) {
-				$scope[$parts[$i]] = array();
-			}
-			$scope = &$scope[$parts[$i]];
-		}
-		$scope[$parts[$i]] = $value;
-	}
+        return current($array);
+    }
 
-	/**
-	 * Merge values in this DotArray instance with values from
-	 * $array. Any conflicting keys will be overwritten by those in
-	 * $array.
-	 *
-	 * @param array $array The array to merge
-	 */
-	public function merge(array $array) {
-		$this->array = array_replace_recursive($this->array, $array);
-	}
+    /**
+     * Set an array value with $key.
+     * $key uses the dot array syntax: parent.child.child.
+     * If $value is an array this will also be accessible using the
+     * dot array syntax.
+     */
+    public function set($key, $value)
+    {
+        $parts = explode('.', $key);
+        //loop through each part, create it if not present.
+        $scope = &$this->array;
+        $count = count($parts) - 1;
+        for ($i = 0; $i < $count; $i++) {
+            if (!isset($scope[$parts[$i]])) {
+                $scope[$parts[$i]] = array();
+            }
+            $scope = &$scope[$parts[$i]];
+        }
+        $scope[$parts[$i]] = $value;
+    }
+
+    /**
+     * Merge values in this DotArray instance with values from
+     * $array. Any conflicting keys will be overwritten by those in
+     * $array.
+     *
+     * @param array $array The array to merge
+     */
+    public function merge(array $array)
+    {
+        $this->array = array_replace_recursive($this->array, $array);
+    }
 
 }
--- tests/Crutches/Tests/DotArrayTest.php
+++ tests/Crutches/Tests/DotArrayTest.php
@@ -9,106 +9,119 @@ use Crutches\DotArray;
  *
  * @author Glynn Forrest <me@glynnforrest.com>
  **/
-class DotArrayTest extends \PHPUnit_Framework_TestCase {
+class DotArrayTest extends \PHPUnit_Framework_TestCase
+{
+    protected $arr = array(
+        'one' => 'one',
+        'two' => array(
+            'one' => 'two.one',
+            'two' => 'two.two'
+        ));
 
-	protected $arr = array(
-		'one' => 'one',
-		'two' => array(
-			'one' => 'two.one',
-			'two' => 'two.two'
-		));
+    public function testConstruct()
+    {
+        $d = new DotArray();
+        $this->assertTrue($d instanceof DotArray);
+        $e = new DotArray($this->arr);
+        $this->assertTrue($e instanceof DotArray);
+    }
 
-	public function testConstruct() {
-		$d = new DotArray();
-		$this->assertTrue($d instanceof DotArray);
-		$e = new DotArray($this->arr);
-		$this->assertTrue($e instanceof DotArray);
-	}
+    public function testGet()
+    {
+        $c = new DotArray($this->arr);
+        $this->assertSame('one', $c->get('one'));
+        $this->assertSame('two.one', $c->get('two.one'));
+    }
 
-	public function testGet() {
-		$c = new DotArray($this->arr);
-		$this->assertEquals('one', $c->get('one'));
-		$this->assertEquals('two.one', $c->get('two.one'));
-	}
+    public function testGetDefault()
+    {
+        $c = new DotArray($this->arr);
+        $this->assertSame('default', $c->get('fake-key', 'default'));
+        $this->assertSame('two.one', $c->get('two.one', 'default'));
+    }
 
-	public function testGetDefault() {
-		$c = new DotArray($this->arr);
-		$this->assertEquals('default', $c->get('fake-key', 'default'));
-		$this->assertEquals('two.one', $c->get('two.one', 'default'));
-	}
+    public function testGetFirst()
+    {
+        $c = new DotArray($this->arr);
+        $this->assertSame('two.one', $c->getFirst('two'));
+        $this->assertSame('one', $c->getFirst());
+    }
 
-	public function testGetFirst() {
-		$c = new DotArray($this->arr);
-		$this->assertEquals('two.one', $c->getFirst('two'));
-		$this->assertEquals('one', $c->getFirst());
-	}
+    public function testGetFirstDefault()
+    {
+        $c = new DotArray($this->arr);
+        $this->assertSame('default', $c->getFirst('fake-key', 'default'));
+        $this->assertSame('two.one', $c->getFirst('two', 'default'));
+    }
 
-	public function testGetFirstDefault() {
-		$c = new DotArray($this->arr);
-		$this->assertEquals('default', $c->getFirst('fake-key', 'default'));
-		$this->assertEquals('two.one', $c->getFirst('two', 'default'));
-	}
+    public function testSet()
+    {
+        $c = new DotArray($this->arr);
+        $c->set('three', 3);
+        $this->assertSame(3, $c->get('three'));
+    }
 
-	public function testSet() {
-		$c = new DotArray($this->arr);
-		$c->set('three', 3);
-		$this->assertEquals(3, $c->get('three'));
-	}
+    public function testSetNested()
+    {
+        $c = new DotArray();
+        $c->set('parent.child', 'value');
+        $this->assertSame(array('parent' => array('child' => 'value')), $c->get());
+    }
 
-	public function testSetNested() {
-		$c = new DotArray();
-		$c->set('parent.child', 'value');
-		$this->assertEquals(array('parent' => array('child' => 'value')), $c->get());
-	}
+    public function testGetNested()
+    {
+        $c = new DotArray($this->arr);
+        $c->set('parent', array('child' => 'value'));
+        $this->assertSame('value', $c->get('parent.child'));
+    }
 
-	public function testGetNested() {
-		$c = new DotArray($this->arr);
-		$c->set('parent', array('child' => 'value'));
-		$this->assertEquals('value', $c->get('parent.child'));
-	}
+    public function testSetDeepNested()
+    {
+        $c = new DotArray();
+        $c->set('parent.child.0.1.2.3.4', 'value');
+        $this->assertSame(array('parent' => array('child' => array(
+            0 => array(1 => array(2 => array(3 => array(4 =>'value'))))))), $c->get());
+    }
 
-	public function testSetDeepNested() {
-		$c = new DotArray();
-		$c->set('parent.child.0.1.2.3.4', 'value');
-		$this->assertEquals(array('parent' => array('child' => array(
-			0 => array(1 => array(2 => array(3 => array(4 =>'value'))))))), $c->get());
-	}
+    public function testGetDeepNested()
+    {
+        $c = new DotArray($this->arr);
+        $c->set('parent', array('child' => array(
+            0 => array(1 => array(2 => array(3 => array(4 =>'value')))))));
+        $this->assertSame('value', $c->get('parent.child.0.1.2.3.4'));
+    }
 
-	public function testGetDeepNested() {
-		$c = new DotArray($this->arr);
-		$c->set('parent', array('child' => array(
-			0 => array(1 => array(2 => array(3 => array(4 =>'value')))))));
-		$this->assertEquals('value', $c->get('parent.child.0.1.2.3.4'));
-	}
+    public function testEmptyGet()
+    {
+        $c = new DotArray($this->arr);
+        $this->assertSame($this->arr, $c->get());
+        $this->assertSame($this->arr, $c->get(null));
+    }
 
-	public function testEmptyGet() {
-		$c = new DotArray($this->arr);
-		$this->assertEquals($this->arr, $c->get());
-		$this->assertEquals($this->arr, $c->get(null));
-	}
+    public function testEmptyGetWithDefault()
+    {
+        $c = new DotArray($this->arr);
+        $this->assertSame($this->arr, $c->get(null, 'default'));
+    }
 
-	public function testEmptyGetWithDefault() {
-		$c = new DotArray($this->arr);
-		$this->assertEquals($this->arr, $c->get(null, 'default'));
-	}
-
-	public function testMerge() {
-		$d = new DotArray($this->arr);
-		$override = array(
-			'three' => 'three',
-			'two' => array(
-				'two' => 'changed'
-			));
-		$expected = array(
-			'one' => 'one',
-			'two' => array(
-				'one' => 'two.one',
-				'two' => 'changed'
-			),
-			'three' => 'three',
-		);
-		$d->merge($override);
-		$this->assertSame($expected, $d->get());
-	}
+    public function testMerge()
+    {
+        $d = new DotArray($this->arr);
+        $override = array(
+            'three' => 'three',
+            'two' => array(
+                'two' => 'changed'
+            ));
+        $expected = array(
+            'one' => 'one',
+            'two' => array(
+                'one' => 'two.one',
+                'two' => 'changed'
+            ),
+            'three' => 'three',
+        );
+        $d->merge($override);
+        $this->assertSame($expected, $d->get());
+    }
 
 }
